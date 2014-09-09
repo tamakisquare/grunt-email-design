37d44089795f72701e13305ff5b0f8e7efd367e1
Colin Huang <windbottle@gmail.com>
2014-09-09 11:04:44 -0700

    [Mod]: Disable modules that we don't need

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,63 +48,63 @@ module.exports = function(grunt) {
         },
 
         // Use Mailgun option if you want to email the design to your inbox or to something like Litmus
-        mailgun: {
-          mailer: {
-            options: {
-              key: 'MAILGUN_KEY', // Enter your Mailgun API key here
-              sender: 'me@me.com', // Change this
-              recipient: 'you@you.com', // Change this
-              subject: 'This is a test email'
-            },
-            src: ['dist/'+grunt.option('template')]
-          }
-        },
+        // mailgun: {
+        //   mailer: {
+        //     options: {
+        //       key: 'MAILGUN_KEY', // Enter your Mailgun API key here
+        //       sender: 'me@me.com', // Change this
+        //       recipient: 'you@you.com', // Change this
+        //       subject: 'This is a test email'
+        //     },
+        //     src: ['dist/'+grunt.option('template')]
+        //   }
+        // },
 
         // Use Rackspace Cloud Files if you're using images in your email
-        cloudfiles: {
-          prod: {
-            'user': 'Rackspace Cloud Username', // Change this
-            'key': 'Rackspace Cloud API Key', // Change this
-            'region': 'ORD', // Might need to change this
-            'upload': [{
-              'container': 'Files Container Name', // Change this
-              'src': 'src/img/*',
-              'dest': '/',
-              'stripcomponents': 0
-            }]
-          }
-        },
+        // cloudfiles: {
+        //   prod: {
+        //     'user': 'Rackspace Cloud Username', // Change this
+        //     'key': 'Rackspace Cloud API Key', // Change this
+        //     'region': 'ORD', // Might need to change this
+        //     'upload': [{
+        //       'container': 'Files Container Name', // Change this
+        //       'src': 'src/img/*',
+        //       'dest': '/',
+        //       'stripcomponents': 0
+        //     }]
+        //   }
+        // },
 
         // CDN will replace local paths with your Cloud CDN path
-        cdn: {
-          options: {
-            cdn: 'Rackspace Cloud CDN URI', // Change this
-            flatten: true,
-            supportedTypes: 'html'
-          },
-          dist: {
-            src: ['./dist/*.html']
-          }
-        }
+        // cdn: {
+        //   options: {
+        //     cdn: 'Rackspace Cloud CDN URI', // Change this
+        //     flatten: true,
+        //     supportedTypes: 'html'
+        //   },
+        //   dist: {
+        //     src: ['./dist/*.html']
+        //   }
+        // }
 
     });
 
     // Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('assemble');
-    grunt.loadNpmTasks('grunt-mailgun');
+    // grunt.loadNpmTasks('grunt-mailgun');
     grunt.loadNpmTasks('grunt-premailer');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-cloudfiles');
+    // grunt.loadNpmTasks('grunt-cloudfiles');
     grunt.loadNpmTasks('grunt-cdn');
 
     // Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['sass','assemble','premailer']);
 
     // Use grunt send if you want to actually send the email to your inbox
-    grunt.registerTask('send', ['mailgun']);
+    // grunt.registerTask('send', ['mailgun']);
 
     // Upload images to our CDN on Rackspace Cloud Files
-    grunt.registerTask('cdnify', ['default','cloudfiles','cdn']);
+    // grunt.registerTask('cdnify', ['default','cloudfiles','cdn']);
 
 };
